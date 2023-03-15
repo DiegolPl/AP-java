bdb5505c9f60f90250fce4210bd564ee1c69200f
DiegolPl <diegopdl1992@gmail.com>
2023-03-15 15:57:12 -0300

    Agrego respuesta al ejercicio 2 de la clase 5

--- Clase5/src/main/java/com/diegolpl/clase5/Clase5.java
+++ Clase5/src/main/java/com/diegolpl/clase5/Clase5.java
@@ -4,20 +4,28 @@ package com.diegolpl.clase5;
 import com.diegolpl.clase5.models.Carrito;
 import com.diegolpl.clase5.models.ItemCarrito;
 import com.diegolpl.clase5.models.Producto;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Clase5 {
 
     public static void main(String[] args) throws Exception {
         Carrito mi_carrito = new Carrito();
         
-        ItemCarrito item1 = new ItemCarrito(1, new Producto("Jabón en polvo",40.0));
-        ItemCarrito item2 = new ItemCarrito(3, new Producto("Esponja",10.0));
-        ItemCarrito item3 = new ItemCarrito(2, new Producto("Chocolate",100.0));
+        Path archivo = Paths.get("C:\\Users\\d13g0\\OneDrive\\Escritorio\\Carpetas - Estudio\\AP - Intro JAVA - UTN\\AP-java\\Clase5\\src\\main\\java\\com\\diegolpl\\clase5\\lista.txt");
+        int count = 0;
         
-        mi_carrito.agregar_item_carrito(item1);
-        mi_carrito.agregar_item_carrito(item2);
-        mi_carrito.agregar_item_carrito(item3);
-        
-        System.out.println(mi_carrito.precio());
+        for(String linea : Files.readAllLines(archivo)){
+            count++;
+            String[] datos = linea.split(" ");
+            
+            if(count != 1){
+                ItemCarrito item = new ItemCarrito(Integer.parseInt(datos[0]), new Producto(datos[2],Double.parseDouble(datos[1])));
+                mi_carrito.agregar_item_carrito(item);
+            }
+            
+        }    
+        System.out.println("Total de mi carrito: $" + mi_carrito.precio());
     }
 }
